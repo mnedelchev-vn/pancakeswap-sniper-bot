1290c5d423c217407f3b2c33fa473e54ef51f49d
Miroslav Nedelchev <miroslavn94@gmail.com>
2021-07-09 12:47:42 +0300

    update bsc mainnet node

--- pancakeswap-sniper-bot.js
+++ pancakeswap-sniper-bot.js
@@ -52,7 +52,7 @@ for (var i = 0, len = params.length; i < len; i+=1) {
 function initPancakeswapSniperBot() {
     bscNetwork = (projectData.utils.propertyExists(args, 'bscNetwork') && allowedNetworks.includes(args.bscNetwork)) ? args.bscNetwork : bscNetwork;
     if (bscNetwork == 'mainnet') {
-        var web3 = new Web3(new Web3.providers.HttpProvider('https://bsc-dataseed1.binance.org:443'));
+        var web3 = new Web3(new Web3.providers.HttpProvider('https://bsc-dataseed.binance.org/'));
         var pancakeContractAddress = '0x10ed43c718714eb63d5aa57b78b54704e256024e';
         var wbnbAddress = '0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c';
         var chainId = 56;
